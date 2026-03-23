4e424a23315195c826d1a99bcfdfebb8e71428ee
Kevin lima pena <kevinlimapena@gmail.com>
2026-03-23 10:38:38 -0400

    AED1 - aula 3 - casas

---
+++ AED1 2025 - aula 3/casasDecimais.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int a;
+    int b;
+    float c;
+
+    a= 5;
+    b=10;
+    c=5.3;
+
+      
+
+    printf("digitou: %d\n" ,a);
+    printf("digitou: %d\n" ,b);
+    printf("digitou: %.3f\n" ,c);
+
+    return 0;
+}
