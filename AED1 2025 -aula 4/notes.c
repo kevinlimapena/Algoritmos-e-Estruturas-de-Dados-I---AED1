9841670c267d4f04a42a2e84577d36e9139877fc
Kevin lima pena <kevinlimapena@gmail.com>
2026-03-25 10:39:38 -0400

    switch case

--- AED1 2025 -aula 4/notes.c
+++ AED1 2025 -aula 4/notes.c
@@ -27,3 +27,54 @@ printf("%d\n",i);
 }
 
 /* a variável i não existe neste ponto do programa */
+
+
+//operador ternario
+
+maior = a > b ? a : b;
+
+
+#include <stdio.h>
+int main(void){
+{
+int a,b,maior;    
+scanf("%d",&a);
+scanf("%d",&b);
+ maior = a > b ? a : b;
+ 
+
+ printf ("maior %d",maior);
+}
+
+}
+
+//switch case c
+
+#include <stdio.h>
+#include <math.h>
+int main(void){
+float num1, num2;
+char op;
+printf ("Digite: numero op numero \n");
+scanf("%f %c %f" , &num1, &op, &num2);
+ 
+switch (op)
+{
+case '+':
+printf (" = %f \n", num1+num2);
+break;
+case '-':
+printf (" = %f \n", num1-num2);
+break;
+case '*':
+printf (" = %f \n", num1*num2);
+break;
+case '/':
+printf (" = %f \n", num1/num2);
+break;
+default :
+printf ("Operador inválido \n");
+break;
+}
+ 
+}
